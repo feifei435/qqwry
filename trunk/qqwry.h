9656aad587733cf52542968021bfba36cf0962ae
surfchen <surfchen@36304623-c044-0410-813c-79886a7bac79>
2008-01-28 03:56:54 +0000

    init

---
+++ trunk/qqwry.h
@@ -0,0 +1,3 @@
+#include <inttypes.h>
+int qqwrt_get_location(char *country,char *area,char *ip,char *wrt_path);
+int qqwrt_get_location_by_long(char *country,char *area,uint32_t ip,char *wrt_path);
