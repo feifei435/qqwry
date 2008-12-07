be9939752a6b76a4a8f7dc101947167f28a700ad
surfchen <surfchen@36304623-c044-0410-813c-79886a7bac79>
2008-12-07 04:44:21 +0000

    mv qqwry.* to libqqwry.*

--- trunk/libs/libqqwry.c
+++ trunk/libs/libqqwry.c
@@ -18,7 +18,7 @@
 #define REDIRECT_TYPE_1 0x01
 #define REDIRECT_TYPE_2 0x02
             
-static uint32_t ip2long(char *ip) {
+static uint32_t ip2long(const char *ip) {
     uint32_t ip_long=0;
     uint8_t ip_len=strlen(ip);
     uint32_t ip_sec=0;
@@ -49,11 +49,11 @@ static uint32_t ip2long(char *ip) {
     }
     return ip_long;
 }
-static uint32_t search_index(uint32_t ip,FILE *qqwrt_file) {
+static uint32_t search_index(const uint32_t ip,FILE *qqwry_file) {
     uint32_t index_ip;
     unsigned char head[8];
     unsigned char index_bytes[7];
-    fread(head,8,1,qqwrt_file);
+    fread(head,8,1,qqwry_file);
     uint32_t index_start,index_end,index_mid;
     index_start = (uint32_t)LE_32(&head[0]);
     index_end = (uint32_t)LE_32(&head[4]);
@@ -69,10 +69,9 @@ static uint32_t search_index(uint32_t ip,FILE *qqwrt_file) {
             index_mid=(index_mid+1)/2;
         }
         index_mid=index_start+index_mid*7;
-        fseek(qqwrt_file,index_mid,SEEK_SET);
-        fread(index_bytes,7,1,qqwrt_file);
+        fseek(qqwry_file,index_mid,SEEK_SET);
+        fread(index_bytes,7,1,qqwry_file);
         index_ip=(uint32_t)LE_32(&index_bytes[0]);
-        //printf("index_ip:%u\n",index_ip);
         if (index_ip==ip) {
             break;
         } else if (index_ip<ip) {
@@ -82,39 +81,37 @@ static uint32_t search_index(uint32_t ip,FILE *qqwrt_file) {
         }
     }
     if (index_ip>ip) {
-        fseek(qqwrt_file,index_start,SEEK_SET);
-        fread(index_bytes,7,1,qqwrt_file);
+        fseek(qqwry_file,index_start,SEEK_SET);
+        fread(index_bytes,7,1,qqwry_file);
     }
     return (uint32_t)LE_24(&index_bytes[4]);
 }
-static int readOrJumpRead(char *location,FILE *qqwrt_file,uint32_t data_index) {
+static int readOrJumpRead(char *location,FILE *qqwry_file,const uint32_t data_index) {
     unsigned char c;
     unsigned char data_index_bytes[3];
     uint32_t jump_data_index=0;
     if (data_index) {
-        fseek(qqwrt_file,data_index,SEEK_SET);
+        fseek(qqwry_file,data_index,SEEK_SET);
     }
-    c=fgetc(qqwrt_file);
+    c=fgetc(qqwry_file);
     switch (c) {
         case REDIRECT_TYPE_2:
         case REDIRECT_TYPE_1:
-            fread(data_index_bytes,3,1,qqwrt_file);
+            fread(data_index_bytes,3,1,qqwry_file);
             jump_data_index=LE_24(&data_index_bytes[0]);
-            fseek(qqwrt_file,jump_data_index,SEEK_SET);
+            fseek(qqwry_file,jump_data_index,SEEK_SET);
             break;
         default:
             location[strlen(location)]=c;
     }
-    while (c=fgetc(qqwrt_file)) {
-        location[strlen(location)]=c;
-    }
-    if (jump_data_index!=0) {
-        fseek(qqwrt_file,data_index+4,SEEK_SET);
+    if (c) {
+        while (c=fgetc(qqwry_file)) {
+            location[strlen(location)]=c;
+        }
     }
     return 1;
-
 }
-static int is_cz88(char *str) {
+static int is_cz88(const char *str) {
     int i;
     int l=strlen(str)-7;
     for (i=0;i<l;i++) {
@@ -132,63 +129,60 @@ static int is_cz88(char *str) {
     }
     return 0;
 }
-int qqwrt_get_location_by_long(char *country,char *area,uint32_t ip,char *wrt_path) {
-    FILE *qqwrt_file;
+int qqwry_get_location_by_long(char *addr1,char *addr2,const uint32_t ip,FILE *qqwry_file) {
     unsigned char data_index_bytes[3];
     uint32_t data_index;
-    uint32_t area_offset;
+    uint32_t addr2_offset;
     unsigned char c;
 
-    qqwrt_file = fopen(wrt_path,"rb");
-    if (!qqwrt_file) {
-        fprintf(stderr,"can not open file:%s",wrt_path);
+    if (!qqwry_file) {
         return 0;
     }
-    data_index = search_index(ip,qqwrt_file);
-    //fprintf(stderr,"index:%u:%u\n",ftell(qqwrt_file),data_index);
+    fseek(qqwry_file,0,SEEK_SET);
+    data_index = search_index(ip,qqwry_file);
+    //fprintf(stderr,"index:%u:%u\n",ftell(qqwry_file),data_index);
 
-    fseek(qqwrt_file,data_index+4,SEEK_SET);
-    switch (c=fgetc(qqwrt_file)) {
-        case REDIRECT_TYPE_1:
-    //fprintf(stderr,"mod_1\n");
-            fread(data_index_bytes,3,1,qqwrt_file);
-            data_index=LE_24(&data_index_bytes[0]);
-            readOrJumpRead(country,qqwrt_file,data_index);
-            //location[strlen(location)]=' ';
-            readOrJumpRead(area,qqwrt_file,0);
-            break;
-        case REDIRECT_TYPE_2:
-    //fprintf(stderr,"mod_2\n");
-            area_offset=data_index+8;
-            fread(data_index_bytes,3,1,qqwrt_file);
+    /*ip 4 + mode byte 1*/
+    fseek(qqwry_file,data_index+4,SEEK_SET);
+    c=fgetc(qqwry_file);
+    if (c==REDIRECT_TYPE_1) {
+        fread(data_index_bytes,3,1,qqwry_file);
+        data_index=LE_24(&data_index_bytes[0]);
+        fseek(qqwry_file,data_index,SEEK_SET);
+        c=fgetc(qqwry_file);
+        /*制造一个假的4bytes位移，抵充ip*/
+        data_index-=4;
+    }
 
-            data_index=LE_24(&data_index_bytes[0]);
-            fseek(qqwrt_file,data_index,SEEK_SET);
-            while (c=fgetc(qqwrt_file)) {
-                country[strlen(country)]=c;
-            }
-            //location[strlen(location)]=' ';
-            readOrJumpRead(area,qqwrt_file,area_offset);
-            break;
-        default:
-    //fprintf(stderr,"mod_d\n");
-            country[strlen(country)]=c;
-            while (c=fgetc(qqwrt_file)) {
-                country[strlen(country)]=c;
-            }
-            while (c=fgetc(qqwrt_file)) {
-                area[strlen(area)]=c;
-            }
+    if (c==REDIRECT_TYPE_2) {
+        /*
+         * ip 4 + mode byte 1 + addr1 offset 3
+         * 这里ip的4个bytes不一定是真的，有可能是上一条注释里提到的情况
+         */
+        addr2_offset=data_index+8;
+        fread(data_index_bytes,3,1,qqwry_file);
+
+        data_index=LE_24(&data_index_bytes[0]);
+        fseek(qqwry_file,data_index,SEEK_SET);
+        while (c=fgetc(qqwry_file)) {
+            addr1[strlen(addr1)]=c;
+        }
+        readOrJumpRead(addr2,qqwry_file,addr2_offset);
+    } else {
+        addr1[strlen(addr1)]=c;
+        while (c=fgetc(qqwry_file)) {
+            addr1[strlen(addr1)]=c;
+        }
+        readOrJumpRead(addr2,qqwry_file,0);
     }
-    if (is_cz88(country)) {
-        country[0]='\0';
+    if (is_cz88(addr1)) {
+        addr1[0]='\0';
     }
-    if (is_cz88(area)) {
-        area[0]='\0';
+    if (is_cz88(addr2)) {
+        addr2[0]='\0';
     }
     return 1;
-} 
-int qqwrt_get_location(char *country,char *area,char *ip,char *wrt_path) {
-    qqwrt_get_location_by_long(country,area,ip2long(ip),wrt_path);
-    return 1;
+}
+int qqwry_get_location(char *addr1,char *addr2,char *ip,FILE *qqwry_file) {
+    return qqwry_get_location_by_long(addr1,addr2,ip2long(ip),qqwry_file);
 }
